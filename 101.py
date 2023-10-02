a3927a2a4b7b42b37cf68b3792d56334cbdaf307
VADLURIVIDHATH <111587094+VADLURIVIDHATH@users.noreply.github.com>
2023-10-02 20:58:32 +0530

    Add files via upload

---
+++ 101.py
@@ -0,0 +1,16 @@
+cricket = "virat kohli"
+print(cricket)
+
+strikerate = "147.9"
+print(strikerate)
+
+games = {
+proffession : "cricketer",   
+name : "virat kohli" ,
+strikeRate : "147.9",
+runs : "2300+",
+centuries : "75+",
+favouriteShot : "cover drive"
+
+}
+print(games[name])
